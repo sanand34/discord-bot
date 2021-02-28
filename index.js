64115685770140c67db5cb38c04f1c4e9200cf09
sanand34 <sanchitanand34@gmail.com>
2021-03-01 00:51:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const axios = require("axios/index");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const TOKEN = "ODE0Nzk1NjQ3MTk4NTYwMzEw.YDjDlA.IPCYXgfYI6_c_Ry-ZvXPKdYtI8A";
+
+bot.login(TOKEN);
+
+bot.on("ready", () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on("message", (msg) => {
+  if (
+    msg.mentions.users.size &&
+    msg.mentions.users.first().username === "_loki"
+  ) {
+    msg.channel.send(`Sanchit is sus`);
+  } else if (msg.content.startsWith("!")) {
+    let search = /[a-z]+/g.exec(msg.content);
+    async function fetchData(search) {
+      const request = await axios.get(
+        `https://api.giphy.com/v1/gifs/search?api_key=${"getapikey"}&q=${search}&limit=25&offset=0&rating=g&lang=en
+        `
+      );
+      msg.channel.send("Le", {
+        files: [request.data.data[0].images.downsized_medium.url],
+      });
+    }
+    fetchData(search[0]);
+  }
+});
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
